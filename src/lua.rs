13dc68c36d73bf64e52568cc63a6dc0af0131ce0
kyren <kerriganw@gmail.com>
2017-09-09 21:29:22 -0400

    Rename 'lua lifetimes to actually be 'lua

--- src/lua.rs
+++ src/lua.rs
@@ -69,15 +69,15 @@ impl<'lua> Value<'lua> {
 }
 
 /// Trait for types convertible to `Value`.
-pub trait ToLua<'a> {
+pub trait ToLua<'lua> {
     /// Performs the conversion.
-    fn to_lua(self, lua: &'a Lua) -> Result<Value<'a>>;
+    fn to_lua(self, lua: &'lua Lua) -> Result<Value<'lua>>;
 }
 
 /// Trait for types convertible from `Value`.
-pub trait FromLua<'a>: Sized {
+pub trait FromLua<'lua>: Sized {
     /// Performs the conversion.
-    fn from_lua(lua_value: Value<'a>, lua: &'a Lua) -> Result<Self>;
+    fn from_lua(lua_value: Value<'lua>, lua: &'lua Lua) -> Result<Self>;
 }
 
 /// Multiple Lua values used for both argument passing and also for multiple return values.
@@ -123,23 +123,23 @@ impl<'lua> DerefMut for MultiValue<'lua> {
 ///
 /// This is a generalization of `ToLua`, allowing any number of resulting Lua values instead of just
 /// one. Any type that implements `ToLua` will automatically implement this trait.
-pub trait ToLuaMulti<'a> {
+pub trait ToLuaMulti<'lua> {
     /// Performs the conversion.
-    fn to_lua_multi(self, lua: &'a Lua) -> Result<MultiValue<'a>>;
+    fn to_lua_multi(self, lua: &'lua Lua) -> Result<MultiValue<'lua>>;
 }
 
 /// Trait for types that can be created from an arbitrary number of Lua values.
 ///
 /// This is a generalization of `FromLua`, allowing an arbitrary number of Lua values to participate
 /// in the conversion. Any type that implements `FromLua` will automatically implement this trait.
-pub trait FromLuaMulti<'a>: Sized {
+pub trait FromLuaMulti<'lua>: Sized {
     /// Performs the conversion.
     ///
     /// In case `values` contains more values than needed to perform the conversion, the excess
     /// values should be ignored. This reflects the semantics of Lua when calling a function or
     /// assigning values. Similarly, if not enough values are given, conversions should assume that
     /// any missing values are nil.
-    fn from_lua_multi(values: MultiValue<'a>, lua: &'a Lua) -> Result<Self>;
+    fn from_lua_multi(values: MultiValue<'lua>, lua: &'lua Lua) -> Result<Self>;
 }
 
 type Callback<'lua> = Box<FnMut(&'lua Lua, MultiValue<'lua>) -> Result<MultiValue<'lua>> + 'lua>;
