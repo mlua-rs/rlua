83669603687358c0bed59405aac6660b0967fe10
kyren <kerriganw@gmail.com>
2018-08-05 11:48:25 -0400

    Update to failure 0.1.2, rename deprecated methods

--- src/error.rs
+++ src/error.rs
@@ -185,10 +185,7 @@ impl failure::Fail for Error {
     fn cause(&self) -> Option<&failure::Fail> {
         match *self {
             Error::CallbackError { ref cause, .. } => Some(cause.as_ref()),
-            // Error::cause simply returns the contained Fail type, which we are already displaying
-            // and returning the backtrace for, no need to repeat it as the cause.
-            // When failure 1.0 is released, this can become `err.as_fail.cause()`
-            Error::ExternalError(ref err) => err.cause().cause(),
+            Error::ExternalError(ref err) => err.as_fail().cause(),
             _ => None,
         }
     }
