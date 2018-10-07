f1893db0272ec5e6f2b77175b7fb20957dc4bbae
Marc Le Bouthillier <chauffretteclimatisee@protonmail.com>
2018-10-07 13:31:59 -0400

    line_counts test adjustment, support for all fields within lua_Debug

--- src/hook.rs
+++ src/hook.rs
@@ -1,11 +1,24 @@
-use libc::c_int;
+use std::{slice, ptr, mem, str};
+use libc::{self, c_int};
 use ffi::{self, lua_State, lua_Debug};
 use lua::extra_data;
 use util::callback_error;
 
 /// Contains information about the running code at the moments specified when setting the hook.
-pub struct Debug {
-    pub curr_line: u32
+#[derive(Clone, Debug)]
+pub struct Debug<'a> {
+    pub name: Option<&'a str>,
+    pub namewhat: Option<&'a str>,
+    pub what: Option<&'a str>,
+    pub source: Option<&'a str>,
+    pub curr_line: u32,
+    pub line_defined: u32,
+    pub last_line_defined: u32,
+    pub num_ups: u32,
+    pub num_params: u32,
+    pub is_vararg: bool,
+    pub is_tailcall: bool,
+    pub short_src: Option<&'a str>
 }
 
 /// Indicates in which circumstances the hook should be called by Lua.
@@ -50,10 +63,27 @@ impl Default for HookOptions {
 /// This callback is passed to `lua_sethook` and gets called whenever debug information is received.
 pub(crate) unsafe extern "C" fn hook_proc(state: *mut lua_State, ar: *mut lua_Debug) {
     callback_error(state, || {
+        if ffi::lua_getinfo(state, cstr!("nSltu"), ar) == 0 {
+            rlua_panic!("lua_getinfo failed")
+        }
+
         let extra = &mut *extra_data(state);
 
         let debug = Debug {
-            curr_line: (*ar).currentline as u32
+            name: ptr_to_str((*ar).name as *const i8),
+            namewhat: ptr_to_str((*ar).namewhat as *const i8),
+            what: ptr_to_str((*ar).what as *const i8),
+            source: ptr_to_str((*ar).source as *const i8),
+            curr_line: (*ar).currentline as u32,
+            line_defined: (*ar).linedefined as u32,
+            last_line_defined: (*ar).lastlinedefined as u32,
+            num_ups: (*ar).nups as u32,
+            num_params: (*ar).nparams as u32,
+            is_vararg: (*ar).isvararg == 1,
+            is_tailcall: (*ar).istailcall == 1,
+            short_src: str::from_utf8(mem::transmute((*ar).short_src.as_ref()))
+                .and_then(|r| Ok(Some(r)))
+                .unwrap_or(None)
         };
 
         let cb = extra.hook_callback
@@ -61,4 +91,13 @@ pub(crate) unsafe extern "C" fn hook_proc(state: *mut lua_State, ar: *mut lua_De
             .expect("no hooks previously set; this is a bug");
         cb(&debug)
     });
+}
+
+unsafe fn ptr_to_str<'a>(input: *const i8) -> Option<&'a str> {
+    if input == ptr::null() || ptr::read(input) == 0 {
+        return None;
+    }
+    let len = libc::strlen(input) as usize;
+    let input = slice::from_raw_parts(input as *const u8, len);
+    str::from_utf8(input).and_then(|r| Ok(Some(r.trim_right()))).unwrap_or(None)
 }
--- tests/hooks.rs
+++ tests/hooks.rs
@@ -1,20 +1,20 @@
 extern crate rlua;
 
 use std::sync::mpsc::{channel, TryRecvError};
-use rlua::{Lua, HookOptions};
+use rlua::{Lua, Debug, HookOptions};
 
 #[test]
 fn line_counts() {
     let code = r#"local x = 2 + 3
     local y = x * 63
-    print(x..", "..y)
+    local z = string.len(x..", "..y)
     "#;
 
     let (sx, rx) = channel();
     let lua = Lua::new();
     lua.set_mut_hook(HookOptions {
         lines: true, ..Default::default()
-    }, move |debug| {
+    }, move |debug: &Debug| {
         let _ = sx.send(debug.curr_line);
         Ok(())
     });
