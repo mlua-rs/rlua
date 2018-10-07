261fa882682944d6ae9d03abd819069ac05d8af8
Marc Le Bouthillier <chauffretteclimatisee@protonmail.com>
2018-10-07 12:39:09 -0400

    Allowed FnMut for hooks, hook test file

---
+++ tests/hooks.rs
@@ -0,0 +1,27 @@
+extern crate rlua;
+
+use std::sync::mpsc::{channel, TryRecvError};
+use rlua::{Lua, HookOptions};
+
+#[test]
+fn line_counts() {
+    let code = r#"local x = 2 + 3
+    local y = x * 63
+    print(x..", "..y)
+    "#;
+
+    let (sx, rx) = channel();
+    let lua = Lua::new();
+    lua.set_mut_hook(HookOptions {
+        lines: true, ..Default::default()
+    }, move |debug| {
+        let _ = sx.send(debug.curr_line);
+        Ok(())
+    });
+    let _: () = lua.exec(code, None).expect("exec error");
+
+    assert_eq!(rx.try_recv(), Ok(1));
+    assert_eq!(rx.try_recv(), Ok(2));
+    assert_eq!(rx.try_recv(), Ok(3));
+    assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+}
