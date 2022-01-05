59f12274d7ba03f9d4ced530c03d80b94a967895
Michael Pollind <mpollind@gmail.com>
2022-01-05 07:24:22 -0800

    chore: correct lua config
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- build.rs
+++ build.rs
@@ -1,24 +1,43 @@
 fn main() {
     let mut lua_version_features = 0;
-    #[cfg(feature = "rlua_lua54")]
+    #[cfg(feature = "builtin-lua54")]
     {
         println!("cargo:rustc-cfg=rlua_lua54");
         lua_version_features += 1;
     }
 
-    #[cfg(feature = "rlua_lua53")]
+    #[cfg(feature = "builtin-lua53")]
     {
         println!("cargo:rustc-cfg=rlua_lua53");
         lua_version_features += 1;
     }
 
-    #[cfg(feature = "rlua_lua51")]
+    #[cfg(feature = "builtin-lua51")]
     {
         println!("cargo:rustc-cfg=rlua_lua51");
         lua_version_features += 1;
     }
+
+    #[cfg(feature = "system-lua54")]
+    {
+        println!("cargo:rustc-cfg=rlua_lua54");
+        lua_version_features += 1;
+    }
+
+    #[cfg(feature = "system-lua53")]
+    {
+        println!("cargo:rustc-cfg=rlua_lua53");
+        lua_version_features += 1;
+    }
+
+    #[cfg(feature = "system-lua51")]
+    {
+        println!("cargo:rustc-cfg=rlua_lua51");
+        lua_version_features += 1;
+    }
+
     if lua_version_features < 1 {
-        panic!("No Lua version specified.  Please enable one of the features.");
+        panic!("No Lua version specified.  Please enable one of the features. use --no-default-features to disable default lua feature.");
     } else if lua_version_features > 1 {
         panic!("Cannot enable more than one Lua interpreter feature.");
     }
