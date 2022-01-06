5e32418421c054feabf3e44a39c56dc1bedcb5c6
Michael Pollind <mpollind@gmail.com>
2022-01-05 20:39:02 -0800

    chore: add lua source to indvidual sys package and update name
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -44,11 +44,11 @@
 mod macros;
 
 #[cfg(rlua_lua51)]
-extern crate lua51_sys as ffi;
+extern crate rlua_lua51_sys as ffi;
 #[cfg(rlua_lua53)]
-extern crate lua53_sys as ffi;
+extern crate rlua_lua53_sys as ffi;
 #[cfg(rlua_lua54)]
-extern crate lua54_sys as ffi;
+extern crate rlua_lua54_sys as ffi;
 mod context;
 mod conversion;
 mod error;
