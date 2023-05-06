fb235a3182401bcfa6c506807702d514a63cad59
Chris Emerson <github@mail.nosreme.org>
2023-05-06 17:40:53 +0100

    Update bindgen script for new bindgen-cli.

--- scripts/update-bindgen.sh
+++ scripts/update-bindgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for crate in crates/rlua-lua??-sys ; do
-    bindgen --size_t-is-usize ${crate}/wrapper_lua*.h -o ${crate}/src/bindings.rs -- -I ${crate}/lua-5.*/src/
+    bindgen ${crate}/wrapper_lua*.h -o ${crate}/src/bindings.rs -- -I ${crate}/lua-5.*/src/
 done
