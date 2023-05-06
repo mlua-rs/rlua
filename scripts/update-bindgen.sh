ca2f74f21a9b4268e8654634b9a69a8d425c8941
Chris Emerson <github@mail.nosreme.org>
2023-05-06 17:49:33 +0100

    Use --sort-semantically with bindgen.

--- scripts/update-bindgen.sh
+++ scripts/update-bindgen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for crate in crates/rlua-lua??-sys ; do
-    bindgen ${crate}/wrapper_lua*.h -o ${crate}/src/bindings.rs -- -I ${crate}/lua-5.*/src/
+    bindgen --sort-semantically ${crate}/wrapper_lua*.h -o ${crate}/src/bindings.rs -- -I ${crate}/lua-5.*/src/
 done
