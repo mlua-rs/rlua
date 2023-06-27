d7a5edfcf8abee1cf10bc47532ba97b6a8d9cb9b
Alexander Schlemmer <alexander@mail-schlemmer.de>
2023-06-27 12:50:09 +0200

    FIX: changes were needed to make repl example work

--- examples/repl.rs
+++ examples/repl.rs
@@ -1,49 +1,56 @@
 //! This example shows a simple read-evaluate-print-loop (REPL).
 
 use rlua::{Error, Lua, MultiValue};
-use rustyline::Editor;
+use rustyline::DefaultEditor;
 
 fn main() {
     Lua::new().context(|lua| {
-        let mut editor = Editor::<()>::new();
-
-        loop {
-            let mut prompt = "> ";
-            let mut line = String::new();
-
-            loop {
-                match editor.readline(prompt) {
-                    Ok(input) => line.push_str(&input),
-                    Err(_) => return,
+        // let mut editor = Editor::<()>::new();
+        let rl_c = rustyline::DefaultEditor::new();
+        match rl_c {
+            Ok(mut editor) => {
+                
+                loop {
+                    let mut prompt = "> ";
+                    let mut line = String::new();
+                    
+                    loop {
+                        match editor.readline(prompt) {
+                            Ok(input) => line.push_str(&input),
+                            Err(_) => return,
+                        }
+                        
+                        match lua.load(&line).eval::<MultiValue>() {
+                            Ok(values) => {
+                                editor.add_history_entry(line);
+                                println!(
+                                    "{}",
+                                    values
+                                        .iter()
+                                        .map(|value| format!("{:?}", value))
+                                        .collect::<Vec<_>>()
+                                        .join("\t")
+                                );
+                                break;
+                            }
+                            Err(Error::SyntaxError {
+                                incomplete_input: true,
+                                ..
+                            }) => {
+                                // continue reading input and append it to `line`
+                                line.push_str("\n"); // separate input lines
+                                prompt = ">> ";
+                            }
+                            Err(e) => {
+                                eprintln!("error: {}", e);
+                                break;
+                            }
+                        }
+                    }
                 }
-
-                match lua.load(&line).eval::<MultiValue>() {
-                    Ok(values) => {
-                        editor.add_history_entry(line);
-                        println!(
-                            "{}",
-                            values
-                                .iter()
-                                .map(|value| format!("{:?}", value))
-                                .collect::<Vec<_>>()
-                                .join("\t")
-                        );
-                        break;
-                    }
-                    Err(Error::SyntaxError {
-                        incomplete_input: true,
-                        ..
-                    }) => {
-                        // continue reading input and append it to `line`
-                        line.push_str("\n"); // separate input lines
-                        prompt = ">> ";
-                    }
-                    Err(e) => {
-                        eprintln!("error: {}", e);
-                        break;
-                    }
-                }
-            }
+                
+            },
+            Err(e) => eprintln!("error: {}", e)
         }
     });
 }
